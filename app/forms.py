c990409728e1b76c022b981d3b408a60d8c8187d
ColinAdo <colinadodev30@gmail.com>
2023-11-15 13:11:46 +0300

    Update TextArea to StringField

--- app/forms.py
+++ app/forms.py
@@ -57,7 +57,7 @@ class EditCommentForm(FlaskForm):
 
 class SettingsFrom(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    about_me = StringField('About me', validators=[Length(min=0, max=140)])
     location = StringField('Location', validators=[Length(min=0, max=20)])
     submit = SubmitField('Submit')
 
