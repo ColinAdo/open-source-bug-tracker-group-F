edc075a9891cb9ce0d6ae1539574c8517114dbcc
ColinAdo <colinadodev30@gmail.com>
2023-11-17 11:21:49 +0300

    Add postgresql db

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # Update the PostgreSQL database URI
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        f'postgresql://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASS")}@localhost/groupf'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
