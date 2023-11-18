dfd2168ffd8813913d7a046172edf4c9d792da57
ColinAdo <colinadodev30@gmail.com>
2023-11-18 12:34:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 import os
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+import os
 
-    # Update the PostgreSQL database URI
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        f'postgresql://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASS")}@localhost/groupf'
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.environ['DBUSER'],
+    dbpass=os.environ['DBPASS'],
+    dbhost=os.environ['DBHOST'],
+    dbname=os.environ['DBNAME']
+)
