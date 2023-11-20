5fe20491b7d6bd3457a4315c7893e6f4a72b4c38
ColinAdo <colinadodev30@gmail.com>
2023-11-20 10:55:59 +0300

    Add azure configs

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,36 @@
+from azureproject.get_conn import get_conn
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 
+import os
+
 app = Flask(__name__)
-app.config.from_object(Config)
+# app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
 app.app_context().push()
-# db.create_all()
+
+# WEBSITE_HOSTNAME exists only in production environment
+if not 'WEBSITE_HOSTNAME' in os.environ:
+   # local development, where we'll use environment variables
+   print("Loading config.development and environment variables from .env file.")
+   app.config.from_object('azureproject.development')
+else:
+   # production
+   print("Loading config.production.")
+   app.config.from_object('azureproject.production')
+
+with app.app_context():
+    app.config.update(
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_DATABASE_URI=get_conn(),
+    )
 
 def truncate_words(text, n):
     words = text.split()
