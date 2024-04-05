80b8e3a703f929ae225e0f45ca679b080a78b78b
ColinAdo <colinadodev30@gmail.com>
2024-04-05 09:38:11 +0300

    Add clean  code to config.py

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    # Update the PostgreSQL database URI
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         f'postgresql://{os.environ.get("DB_USER")}:{os.environ.get("DB_PASS")}@localhost/groupf'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
